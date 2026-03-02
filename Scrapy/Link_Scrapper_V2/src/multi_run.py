3e36510382aea774333481f1a04d1982aaf89cc4
bruen-smith <bruensmith541@gmail.com>
2026-03-02 10:36:41 -0800

    Refactor multi_run.py for dynamic process count

--- Scrapy/Link_Scrapper_V2/src/multi_run.py
+++ Scrapy/Link_Scrapper_V2/src/multi_run.py
@@ -1,20 +1,11 @@
 import subprocess
+import os
 from multiprocessing import Pool
 
 # List of websites to check
 WEBSITES = [
-    "https://www.bethel.k12.or.us",
-    "https://cascade.bethel.k12.or.us",
-    "https://clearlake.bethel.k12.or.us",
-    "https://danebo.bethel.k12.or.us",
-    "https://fairfield.bethel.k12.or.us",
-    "https://irving.bethel.k12.or.us",
-    "https://malabon.bethel.k12.or.us",
-    "https://shasta.bethel.k12.or.us",
-    "https://willamette.bethel.k12.or.us",
-    "https://meadowview.bethel.k12.or.us",
-    "https://kalapuya.bethel.k12.or.us",
-    "https://prairiemountain.bethel.k12.or.us",
+    "https://www.website.com",
+    "https://www.website2.com"
 ]
 
 PYTHON_EXECUTABLE = ".venv/bin/python"  # Path to Python executable
@@ -29,8 +20,10 @@ def check_site(site):
 
 if __name__ == "__main__":
     with Pool(
-        processes=100
+        cpu = os.cpu_count()
+        processes = min(32, cpu * 5)
     ) as pool:  # You can adjust the number of processes based on cores
         pool.map(check_site, WEBSITES)
 
     print("All sites checked.")
+
