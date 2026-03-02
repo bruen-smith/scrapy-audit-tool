408cb86bcf7e7896c73cd37d3d93be085fe9d784
bruen-smith <bruensmith541@gmail.com>
2026-03-02 10:17:12 -0800

    Update website list in multi_run.py

--- Scrapy/Link_Scrapper_V1/src/multi_run.py
+++ Scrapy/Link_Scrapper_V1/src/multi_run.py
@@ -3,18 +3,8 @@ from multiprocessing import Pool
 
 # List of websites to check
 WEBSITES = [
-    "https://www.bethel.k12.or.us",
-    "https://cascade.bethel.k12.or.us",
-    "https://clearlake.bethel.k12.or.us",
-    "https://danebo.bethel.k12.or.us",
-    "https://fairfield.bethel.k12.or.us",
-    "https://irving.bethel.k12.or.us",
-    "https://malabon.bethel.k12.or.us",
-    "https://shasta.bethel.k12.or.us",
-    "https://willamette.bethel.k12.or.us",
-    "https://meadowview.bethel.k12.or.us",
-    "https://kalapuya.bethel.k12.or.us",
-    "https://prairiemountain.bethel.k12.or.us",
+    "https://www.website.com",
+    "https://www.website2.com"
 ]
 
 PYTHON_EXECUTABLE = ".venv/bin/python"  # Path to Python executable
@@ -34,3 +24,4 @@ if __name__ == "__main__":
         pool.map(check_site, WEBSITES)
 
     print("All sites checked.")
+
